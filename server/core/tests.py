afb84413b16a0a18037d9f1f0e47aa082814a145
garciamendes <garciaprog0101@gmail.com>
2025-11-19 15:03:49 -0300

    fix: ajustes nos testes

--- server/core/tests.py
+++ server/core/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 
 
-# Create your tests here.
 class SimpleTest(TestCase):
     def test_addition(self):
-        self.assertEqual(1 + 2, 2)
+        self.assertEqual(1 + 2, 3)
